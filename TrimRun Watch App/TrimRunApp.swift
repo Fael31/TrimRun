b9ca1c277fa56652316724a812f47f37f74be022
Muhammad Fauzul Akbar <muhammadfauzulakbar@gmail.com>
2023-05-19 09:47:54 +0700

    Initial Commit

---
+++ TrimRun Watch App/TrimRunApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrimRunApp.swift
+//  TrimRun Watch App
+//
+//  Created by Muhammad Fauzul Akbar on 19/05/23.
+//
+
+import SwiftUI
+
+@main
+struct TrimRun_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
